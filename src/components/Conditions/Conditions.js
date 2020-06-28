aaeb7ddf01571c795749fa8c038d9f6458d3d3ad
Nicholas Rode <nrode@MBPNR.hsd1.wa.comcast.net>
2020-06-28 13:48:33 -0700

    Cors issues

--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import classes from './Conditions.module.css';
 
 const conditions = (props) => {
     return (
-        <div>
+        <div className={classes.Wrapper}>
+
+            {props.error && <small className={classes.Small}>Please enter a valid city.</small>}
+
+            {props.loading && <div className={classes.Loader} />}
+
             {props.responseObj.cod === 200 ?
                 <div>
                     <p><strong>{props.responseObj.name}
                     </strong></p>
-                    <p>It is currently
-        {Math.round(props.responseObj.main.temp)} degrees out with
-        {props.responseObj.weather[0].description}.</p>
+                    <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
                 </div>
-                : null
+            : null
             }
-        </div>
+        </div>  
     )
 }
 
--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -3,36 +3,61 @@ import Conditions from '../Conditions/Conditions';
 import classes from './Forecast.module.css';
 
 const Forecast = () => {
+
     let [city, setCity] = useState('');
     let [unit, setUnit] = useState('imperial');
     let [responseObj, setResponseObj] = useState({});
-    const uriEncodedCity = encodeURIComponent(city);
+    let [error, setError] = useState(false);
+    let [loading, setLoading] = useState(false);
+
     function getForecast(e) {
         e.preventDefault();
-        fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
+
+        if (city.length === 0) {
+            return setError(true);
+        }
+
+        setError(false);
+        setResponseObj({});
+       
+        setLoading(true);
+       
+        const uriEncodedCity = encodeURIComponent(city);
+     fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-                "x-rapidapi-key": "76b39e4867msh9c1a9f3b7db75eap1ab006jsn1d26bae1cbfd"
+                "x-rapidapi-key": process.env.REACT_APP_API_KEY
             }
         })
-            .then(response => response.json())
-            .then(response => {
-                setResponseObj(response)
-            });
-    }
+        .then(response => response.json())
+        .then(response => {
+            if (response.cod !== 200) {
+                throw new Error()
+            }
+
+            setResponseObj(response);
+            setLoading(false);
+        })
+        .catch(err => {
+            console.log("Here's the err", err)
+            setError(true);
+            setLoading(false);
+            console.log(err.message);
+        });
+     }
+
     return (
         <div>
             <h2>Find Current Weather Conditions</h2>
-            <form onSubmint={getForecast}>
+            <form onSubmit={getForecast}>
                 <input 
                     type="text"
                     placeholder="Enter City"
                     maxLength="50"
                     className={classes.textInput}
                     value={city}
-                    onChange={(e) =>
-                    setCity(e.target.value)}
+                    onChange={(e) => setCity(e.target.value)}
                     />
                 <label className={classes.Radio}>
                     <input 
@@ -40,9 +65,8 @@ const Forecast = () => {
                         name="units"
                         checked={unit === "imperial"}
                         value="imperial"
-                        onChange={(e) =>
-                        setUnit(e.target.value)}
-                    />
+                        onChange={(e) => setUnit(e.target.value)}
+                        />
                     Fahrenheit
                 </label>
                 <label className={classes.Radio}>
@@ -51,16 +75,17 @@ const Forecast = () => {
                         name="units"
                         checked={unit === "metric"}
                         value="metric"
-                        onChange={(e) =>
-                        setUnit(e.target.value)}
-                    />
+                        onChange={(e) => setUnit(e.target.value)}
+                        />
                     Celcius
                 </label>
                     
-                    <button className={classes.Button} type="submit">Get Forecast</button>
+                <button className={classes.Button} type="submit">Get Forecast</button>
             </form>
             <Conditions
                 responseObj={responseObj}
+                error={error}
+                loading={loading}
             />
         </div>
     )
