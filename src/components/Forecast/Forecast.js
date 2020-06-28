4c939cb6d2b8b7c3837c61a69a39ea0b9c8e0690
Nicholas Rode <nrode@MBPNR.hsd1.wa.comcast.net>
2020-06-28 11:00:00 -0700

    Update Forcast Component

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import Conditions from '../Conditions/Conditions';
 
 const Forecast = () => {
+    let [city, setCity] = useState('');
+    let [unit, setUnit] = useState('imperial');
     let [responseObj, setResponseObj] = useState({});
+    const uriEncodedCity = encodeURIComponent(city);
     function getForecast() {
-        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=seattle", {
+        fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
@@ -19,7 +22,40 @@ const Forecast = () => {
     return (
         <div>
             <h2>Find Current Weather Conditions</h2>
-            <button onClick={getForecast}>Get Forecast</button>
+            <form onSubmint={getForecast}>
+                <input 
+                    type="text"
+                    placeholder="Enter City"
+                    maxLength="50"
+                    value={city}
+                    onChange={(e) =>
+                    setCity(e.target.value)}
+                    />
+                <label>
+                    <input 
+                        type="radio"
+                        name="units"
+                        checked={unit === "imperial"}
+                        value="imperial"
+                        onChange={(e) =>
+                        setUnit(e.target.value)}
+                    />
+                    Fahrenheit
+                </label>
+                <label>
+                    <input 
+                        type="radio"
+                        name="units"
+                        checked={unit === "metric"}
+                        value="metric"
+                        onChange={(e) =>
+                        setUnit(e.target.value)}
+                    />
+                    Celcius
+                </label>
+                    
+                    <button type="submit">Get Forecast</button>
+            </form>
             <Conditions
                 responseObj={responseObj}
             />
