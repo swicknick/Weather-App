64bc44767fb8e5c268214cc6502c793d59723226
Nicholas Rode <nrode@MBPNR.hsd1.wa.comcast.net>
2020-06-30 14:45:51 -0700

    MVP Working version

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Weather App</h1>
+        <h1>Weather</h1>
       </header>
       <main>
         <Forecast />
